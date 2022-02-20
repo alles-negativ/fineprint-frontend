f7ac0edb9d605fd0bd815909ea46b801fbf15c2a
Aurelian Ammon <aurelianammon@gmail.com>
2022-02-20 23:55:12 +0100

    [fix]: cookie consent banner

--- components/Header/Header.vue
+++ components/Header/Header.vue
@@ -1,18 +1,33 @@
 <template>
-  <div :class="{ 'home': $route.name === 'index' }" class="spacer">
-    <div class="header__wrapper">
-      <CookieBox />
-      <div class="line"></div>
-      <div class="header">
-        <Navigation />
+  <div>
+    <div :class="{ 'home': $route.name === 'index' }" class="spacer">
+      <div class="header__wrapper">
+        <CookieBox @cookie="updateCookie"></CookieBox>
+        <div class="line"></div>
+        <div class="header">
+          <Navigation />
+        </div>
       </div>
     </div>
+    <div v-bind:class="{ 'isActive': this.cookie }" class="cookie_spacer"></div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Header',
+
+  data() {
+    return {
+      cookie: false
+    }
+  },
+  methods: {
+    updateCookie(e) {
+      this.cookie = e;
+      console.log(e);
+    },
+  }
 }
 </script>
 
